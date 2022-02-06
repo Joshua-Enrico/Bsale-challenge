724fcab7f664a4a661b6bc7bb19861fea53e360e
yoyogold-a11 <tmrfack@gmail.com>
2022-02-06 02:41:37 -0500

    firt commit. setting up endpoints and searcho algorithm

---
+++ api/src/index.js
@@ -0,0 +1,22 @@
+/* Este es el modulo iniciamos express */
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+/* Este es el modulo iniciamos express */
+const CategoryRoute = require('./routes/category');
+const ProductRoute = require('./routes/products');
+
+
+/* inicializamos express y declaramos los enpoints que usaremos */
+const api = express();
+api.use(express.json());
+api.use("/api/category", CategoryRoute);
+api.use("/api/products", ProductRoute);
+/* end  */
+
+/* declaramos el puerto proporcionado o usamos uno por default */
+api.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running on port ' + process.env.PORT);
+});
+
---
+++ api/src/utils/database.js
@@ -0,0 +1,31 @@
+/* En este modulos vamos inicializamos nuestro orm y,
+   conectamos con la base de datos proporcionada */
+
+const { Sequelize } = require('sequelize');// Importamos sequelize(ORM)
+const dotenv = require('dotenv'); // Importamos dotenv para leer nuestras variables de entorno
+dotenv.config();
+
+
+/* Aqui inicializamos la conexión con la base de datos de MySQL usando sequelize(orm) */
+const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
+  host: process.env.host,
+  dialect: "mysql" /* selecting dialect */
+});
+
+sequelize
+  .authenticate(process.env.username)
+  .then(() => {
+    console.log('Connection has been established successfully.'); // eslint-disable-line no-console
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err); // eslint-disable-line no-console
+  });
+
+
+/* usaremos este objeto para exportarlo y poder usarlo,
+   en otros archivos de manera ordenada*/
+const db = {}
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
