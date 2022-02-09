3958166bdacc244a35639c812e25c3dccbe2f277
yoyogold-a11 <tmrfack@gmail.com>
2022-02-08 19:50:27 -0500

    implementacion de los search calls

--- front/apicalls/shopCalls.js
+++ front/apicalls/shopCalls.js
@@ -1,26 +1,90 @@
+const url = `http://127.0.0.1:5500/front/pages/sproduct.html?id=`;
 
-    const getProducts = (req, res) => {
-        axios.get('http://localhost:3000/api/products/qty/8')
+
+function getParams(key = null) {
+    let currentUrl = window.location.search;
+    let url = new URLSearchParams(currentUrl);
+    if (key) {
+        return url.get(key);
+    }
+
+}
+
+
+const getProductByid = (selectArg) => {
+    let arg = getParams('arg')
+
+    if (selectArg){
+        arg = selectArg
+    }
+
+    let content = '';
+    let pages = '';
+
+    if (arg) {
+        axios.get('http://localhost:3000/api/products/search/' + arg)
             .then(response => {
-                const products = response.data;
-                products.map(({id, name, url_image, price, discount, category}) => {
-                    $('#product-container').append(`
+                const element = document.getElementById('product-container');
+                const products = response.data.rows;
+                pages = Math.ceil(response.data.count / 8);
+                Pagination(pages, arg);
+                products.map(({ id, name, url_image, price, discount, category }) => {
+
+                    content +=`
                     <div class="pro">
                         <img src="${url_image}" alt="">
                         <div class="des">
-                            ${
-                                discount ? `<span class="price">$${price} de descuento</span>` : ""
-                            }
-                            <h5>${name}}</h5>
-                            <h4>$${price}}</h4>
+                            ${discount ? `<span class="price">${discount}% de descuento</span>` : ""
+                        }
+                            <h5>${name}</h5>
+                            <h4>$${price}</h4>
                         </div>
-                        <a href="#"><i class="fas fa-shopping-cart cart"></i></a>
+                        <a href="${url + id}"><i class="fas fa-shopping-cart cart"></i></a>
                     </div>
-                    `)
-                })
+                    `;
+                });
+                element.innerHTML = content;
+                console.log(element)
             }).catch(err => {
                 console.log(err);
             })
     }
+}
+getProductByid();
 
-getProducts();
+
+// funcion para llamar productos por paginacion
+const getProductsByPage = ( page, size, arg) => {
+
+    axios.get(`http://localhost:3000/api/products/search/paginate/${arg}?page=${page}&size=${size}`)
+        .then(response => {
+            const element = document.getElementById('product-container');
+            const products = response.data.rows;
+            let content = '';
+            products.map(({ id, name, url_image, price, discount, category }) => {
+
+                content += `<div class="pro">
+                <img src="${url_image}" alt="">
+                <div class="des">
+                    ${discount ? `<span class="price">${discount}% de descuento</span>` : ""
+                    }
+                    <h5>${name}</h5>
+                    <h4>$${price}</h4>
+                </div>
+                <a href="${url + id}"><i class="fas fa-shopping-cart cart"></i></a>
+            </div>`;
+            })
+            element.innerHTML = content;
+            console.log(element)
+        }).catch(err => {
+            console.log(err);
+        })
+}
+
+
+
+$("#select").change(function () {
+    
+    const arg = $(this).val()
+    getProductByid(arg);
+  });
