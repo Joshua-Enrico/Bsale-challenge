bb6900afd70ac2436a383197f87644e29f60e5f3
yoyogold-a11 <tmrfack@gmail.com>
2022-02-09 20:29:07 -0500

    fixing path

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,4 +3,5 @@
 sudo chmod -R 777 /home/api/
 cd /home/api/api/
 npm install
-node index.js > app.out.log 2> app.err.log < /dev/null & 
+cd src
+node index.js > app.out.log 2> app.err.log < /dev/null &
