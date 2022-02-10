51219f4e82b5689d61e1cdb113ee3b567009b002
yoyogold-a11 <tmrfack@gmail.com>
2022-02-09 20:15:53 -0500

    fixing

--- scripts/befoe_install.sh
+++ scripts/befoe_install.sh
@@ -3,8 +3,8 @@
 #create our working directory if it doesnt exist
 DIR="/home/api/express-app"
 if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
+  sudo echo "${DIR} exists"
 else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+  sudo echo "Creating ${DIR} directory"
+  sudo mkdir ${DIR}
 fi
