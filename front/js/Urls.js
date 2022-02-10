086e92d8bc5b7d52f3adbb288aba36ee53a45f69
yoyogold-a11 <tmrfack@gmail.com>
2022-02-09 21:57:44 -0500

    new urls

--- front/js/Urls.js
+++ front/js/Urls.js
@@ -1,2 +1,2 @@
-const ApiUrl = "http://localhost:3000"
-const PageUrl = "http://localhost:5500/front/pages"
+const ApiUrl = "http://54.211.177.150:3000/"
+const PageUrl = "https://bsale-challenge-front.vercel.app/front/pages"
